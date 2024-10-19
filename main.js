a73ef71fa461460a74d8d7abff7993fb815c8854
michael.maina <michaelmainacodes@gmail.com>
2024-10-19 13:56:41 +0300

    Just over 50% complete

--- main.js
+++ main.js
@@ -13,3 +13,36 @@ navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 })
+
+const scrollRevealOption = {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay:500,
+});
+ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay:1000,
+});
+ScrollReveal().reveal(".header__btns", {
+    ...scrollRevealOption,
+    delay:1500,
+});
+
+const banner = document.querySelector(".banner__container");
+
+const bannerContent = Array.from(banner.children);
+
+bannerContent.forEach((item) => {
+    const duplicateNode = item.cloneNode(true)
+    duplicateNode.setAttribute("aria-hidden", true)
+    banner.appendChild(duplicateNode);
+});
